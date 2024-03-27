b61e94a48ac38d1c08bf29bb5613a4769e076ce1
nekrean81 <130750045+nekrean81@users.noreply.github.com>
2024-03-27 18:11:20 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,39 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 3.0.0"
-    }
-  }
-  required_version = ">= 0.14.9"
-}
-provider "azurerm" {
-  features {}
+
+required_providers {
+
+azurerm = {
+
+source = "hashicorp/azurerm"
+
+version = "~> 3.0.0"
+
 }
 
+}
+
+required_version = ">= 0.14.9"
+
+}
+
+provider "azurerm" {
+
+features {}
+
+}
+
+
+
+# The following example shows how to generate a random priority
+# between 1 and 50000 for a aws_alb_listener_rule resource:
 
 resource "random_integer" "priority" {
   min = 1
   max = 50000
-  keepers = {
-    # Generate a new integer each time we switch to a new listener ARN
-    listener_arn = var.listener_arn
-  }
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example"
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-bruce-${random_integer.priority.result}"
   location = "West Europe"
 }
