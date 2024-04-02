62925fd59220a2346eb4143e4c2abff61b668032
nekrean81 <130750045+nekrean81@users.noreply.github.com>
2024-04-02 16:27:01 -0300

    5commite

---
+++ gerer-parc-it-app/gerer-parc-it-app/src/main/java/com/erfei/gererparcitapp/GererParcItAppApplication.java
@@ -0,0 +1,13 @@
+package com.erfei.gererparcitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GererParcItAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GererParcItAppApplication.class, args);
+	}
+
+}
