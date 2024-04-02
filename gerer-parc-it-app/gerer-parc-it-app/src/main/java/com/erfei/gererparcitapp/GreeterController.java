9746ecec943357c12bde2c10996b7e8b8da65cde
nekrean81 <130750045+nekrean81@users.noreply.github.com>
2024-04-02 13:15:47 -0300

    Add files via upload

---
+++ gerer-parc-it-app/gerer-parc-it-app/src/main/java/com/erfei/gererparcitapp/GreeterController.java
@@ -0,0 +1,14 @@
+package com.erfei.gererparcitapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreeterController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Quentin";
+    }
+}
+
